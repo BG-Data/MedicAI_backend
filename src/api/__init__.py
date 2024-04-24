beaed5acb817a2f527080a4dc10fa667896a6ae3
Leon Balloni <leon.balloni@makes.ai>
2024-04-23 23:44:32 -0300

    specific schema for insert and update

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -68,7 +68,6 @@ class UserApi(CrudApi):
         offset: int = 0,
         get_schema: Request = None,
         session: Session = Depends(get_session),
-        user_context=Depends(AuthService.get_auth_user_context),
     ):
         try:
             user_data = await super().get(id, limit, offset, get_schema, session)
@@ -76,3 +75,48 @@ class UserApi(CrudApi):
         except Exception as exp:
             logger.error(f"error at get {self.__class__.__name__} {exp}")
             raise HTTPException(status_code=400, detail=str(exp))
+
+    def insert(
+        self, insert_schema: UserInsert, session: Session = Depends(get_session)
+    ):
+        "Ordinary user -> buyer"
+        try:
+            insert_schema.password = self.password_service.hash_password(
+                insert_schema.password
+            )
+            return (
+                super().insert(insert_schema, session).model_dump(exclude={"password"})
+            )
+        except Exception as exp:
+            logger.error(f"error at insert {self.__class__.__name__} {exp}")
+
+    def update(
+        self,
+        id: int,
+        update_schema: MakeOptionalPydantic.make_partial_model(UserUpdate),
+        session: Session = Depends(get_session),
+    ):
+
+        if update_schema.model_dump(exclude_unset=True).get("old_password"):
+            valid = self.password_service.get_password(
+                update_schema.old_password,
+                self.crud.get_itens({"id": id}, session)[0].password,
+            )
+            update_schema.old_password = None
+            update_schema.password = self.password_service.hash_password(
+                update_schema.password
+            )
+            if not valid:
+                raise HTTPException(
+                    status_code=401,
+                    detail={
+                        "status_code": status.HTTP_401_UNAUTHORIZED,
+                        "info": "The given password is not valid",
+                    },
+                )
+        try:
+            return self.crud.update_item(id, update_schema, session).model_dump(
+                exclude={"password"}
+            )
+        except Exception as exp:
+            logger.error(f"error at update {self.__class__.__name__} {exp}")
